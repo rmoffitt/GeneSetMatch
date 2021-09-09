7f111171047ed39a8dde11c863cc913f9140b934
lchrastecka <lucie.chrastecka@stonybrook.edu>
2021-09-09 19:08:55 +0000

    Adding Mouse homologue conversion function ENSEMBLE to ENTREZ

---
+++ R/MouseENS2MouseENT.R
@@ -0,0 +1,26 @@
+#' Convert mouse ENSEMBLE ID's to mouse ENTREZ ID's + MGI's
+#' @export
+#' @import biomaRt
+#' @import data.table
+#' @import msigdbr
+#' @import biomaRt
+#' @import org.Hs.eg.db
+#' @import org.Mm.eg.db
+#' @param MouseGenes is a vector of mouse ENSEMBLE ID's
+#' @return nothing
+#' @examples 
+#' readcounts <- data$ex
+#' mouseENS <- data$featInfo$ENSEMBL
+#' mouseENT <- MouseENS2MouseENT(MouseGenes)
+
+MouseENS2MouseENT <- function(MouseGenes){
+  mouseENT = useMart("ensembl", dataset = "mmusculus_gene_ensembl", host = "uswest.ensembl.org")
+  mouseENS = useMart("ensembl", dataset = "mmusculus_gene_ensembl", host = "uswest.ensembl.org")
+  genesMouseENS2MouseENT = getBM(attributes = c("entrezgene_id", "ensembl_gene_id", "mgi_symbol"),
+                             filters = "ensembl_gene_id", 
+                             values = MouseGenes, 
+                             mart = mouseENS,
+                             uniqueRows = TRUE)
+  colnames(genesMouseENS2MouseENT) <- c("ENTREZID", "ENSEMBL", "SYMBOL")
+  return(genesMouseENS2MouseENT) 
+}
