d25e1e796c9ad9a54905e8f82965cf12b0571652
lchrastecka <lucie.chrastecka@stonybrook.edu>
2021-10-26 16:15:06 +0000

    Building a new heatmap function which visualizes nmf vectors with multiple colors.

--- R/ODIS.NMF.Heatmaps.R
+++ R/ODIS.NMF.Heatmaps.R
@@ -2,113 +2,62 @@
 #gotta find a way to display all represented pathwars in one gmt file and all leading edges from V1, V2, V3 in one plot. Use three colors. 
 # prpobably gotta restucture output withing a new heatmpa function
 
+#
+
+
+
 
 ODIS.NMF.Heatmaps <- function(gsea_results){
   
-  my_palette <- colorRampPalette(c("blue", "lightgrey", "red"))(n = 299)
+  #my_palette <- colorRampPalette(c("blue", "lightgrey", "red"))(n = 299) #MAYBE NOT YET?
   plots <- list()
   
-  for(i in names(gsea_results)){
+  #this loop extracts top 30genes from each gmt.file and their top 30 associated pathways (16-39)
+  for(i in names(gsea_results[[1]])){#FOR EACH LEVEL 1, PULL OBJECT Vx
     thisAnalysis <- gsea_results[[i]]
-    plots[[i]] <- list()
-    for(k in (1:(length(thisAnalysis) - 1))){
-      theseResults <- thisAnalysis[[k]]$Results
+    agg_leading_edge = list() #ALL LEADCING EDGE GENES OF TOP 30 PATHWAYS
+    leading_sets = character() #TOP 30 PATHWAYS FROM EACH GMT FILE
+    for(k in names(gsea_results)){
       
-      if (length(theseResults$leadingEdge) > 40){
-        theseResults <- theseResults[c(order(theseResults$ES)[1:30],
-                                       order(-theseResults$ES)[1:30]),]
-      }
-      
-      
-      all.genes <- unique(c(unlist(theseResults$leadingEdge)))
-      
+      #make matrix and then glue smaller matrices together
       theMatrix <- matrix(data = 0,
-                          nrow = length(theseResults$leadingEdge),
-                          ncol = length(all.genes))
+                          nrow = length(leading_sets),
+                          ncol = length(c(agg_leading_edge$V1$ENTREZID, agg_leading_edge$V2$ENTREZID, agg_leading_edge$V3$ENTREZID)))
+      #I have top 30 enriched pathways
+      orig_matrix = gsea_results[[k]]$orig_matrix #pull out and separate fpr appropriate k every time
+      theseResults <- gsea_results[[k]][[i]]$Results[1:30,] #already sorted by normalized enrichment score
       
-      rownames(theMatrix) <- theseResults$pathway
+      plots[[i]] <- list()
       
-      theIndexWeWant <- match(
-        as.character(all.genes),
-        thisAnalysis$orig_matrix$ENTREZID) #this should be thisAnalysis$`./c1.all.v7.1.entrez.gmt`$Results$leadingEdge
-      colnames(theMatrix) <- thisAnalysis$orig_matrix$SYMBOL[theIndexWeWant]
+      # Take all the leadsing edge genes from theseResults
+      theEdge = unique(unlist(theseResults$leadingEdge))
+      # returns the row index of theEdge vs orig_matrix, which is sorted by log2FoldChange (decreasing)
+      theTopGenes = theEdge[order(match(theEdge, orig_matrix$ENTREZID))][1:30]
+      agg_leading_edge[[k]] = orig_matrix[orig_matrix$ENTREZID %in% theTopGenes,c("log2FoldChange","ENTREZID")]
+      print(length(theTopGenes))
+  
+      # agg_leading_edge = c(agg_leading_edge,
+      #                      theTopGenes)
+      # 
+      # print(length(agg_leading_edge))
+      
+      leading_sets = c(leading_sets, theseResults$pathway)
+    }
+      #pull out orix_matrix log2fold change associated with all 90 (15-40)
+      
+      #do this better
+      
+      rownames(theMatrix) <- leading_sets
+      colnames(theMatrix) <- c(agg_leading_edge$V1$ENTREZID, agg_leading_edge$V2$ENTREZID, agg_leading_edge$V3$ENTREZID)
+      
+      #fill TheMatrix with logfoldchange values
       
       plots[[i]][[k]] <- list()
       
-      if(length(theseResults$leadingEdge) > 1) {
-        for(j in 1:length(theseResults$leadingEdge)){
-          print(j)
-          theseLeadingGenes <- theseResults$leadingEdge[[j]]
-          if (length(theseLeadingGenes) > 0){
-            
-            #where are these in my heatmap?
-            firstIndexWeWant <- match(as.character(theseLeadingGenes),
-                                      thisAnalysis$orig_matrix$ENTREZID[theIndexWeWant])
-            
-            #where are these in original data frame?
-            secondIndexWeWant <- match(as.character(theseLeadingGenes), 
-                                       thisAnalysis$orig_matrix$ENTREZID)
-            
-            theMatrix[j, firstIndexWeWant] <- thisAnalysis$orig_matrix$log2FoldChange[secondIndexWeWant] 
-          } #error here
-        }
-        
-        print("dim of Matrix before sorting keepers")
-        print(dim(theMatrix))
-        
-        #Here is my full matrix
-        if(ncol(theMatrix) > 80){
-          print("Too many columns!")
-          movers = as.numeric(apply(abs(theMatrix),1,which.max)) #returns col index of largest moving gene PER ROW
-          overall = abs(colSums(theMatrix)) #takes colSums
-          print(range(overall))
-          overall = sort(overall, decreasing = T)[1:80] #sorts big to small and takes top 80
-          keepers = unique(c(which(colnames(theMatrix) %in% names(overall)), movers)) # combines top movers from each set ('movers') with consensus genes ('overall'), unique() removes overlap
-          theMatrix = theMatrix[, keepers] #only keeps columns that are present in 'overall'
-          
-          # I've now ensured all rows have at least one non-zero, so the below is now irrelevant??
-          ind = which(rowSds(theMatrix) == 0) # Finds which rows are now all 0s (sd == 0)
-          print(paste0("Num Rows with zero SD: ", length(ind)))
-          #theMatrix = theMatrix[ind,] # Keeps only rows where rowSD is not equal to 0, allows for clustering
-        }
-        print("dim of Matrix after sorting")
-        print(dim(theMatrix))
-        
-        
-        pdf(paste0(i, "_", substr(names(thisAnalysis)[k],
-                                  5, stop = 999), ".pdf"),
-            width = ncol(theMatrix)/10 + 5,
-            height= nrow(theMatrix)/10 + 5)
-        
-        
-        
-        
-        plots[[i]][[k]] <- heatmap.2(theMatrix,
-                                     main = paste0(i, "_", substr(names(thisAnalysis)[k], 5, stop = 999)),
-                                     distfun =  function(x) {
-                                       d <- (1-cor(t(x))) + as.matrix(pdist(sign(rowMeans(x))))
-                                       return(as.dist(d))},
-                                     density.info = "density",
-                                     trace = "none",
-                                     margins = c(3,20),
-                                     cexRow = 0.7,
-                                     cexCol = 0.5,
-                                     col = my_palette,
-                                     breaks = (-149:150-0.5)/50,
-                                     dendrogram = "both",
-                                     #offsetRow = -205,
-                                     #lmat = rbind(c(1,2), c(3,4), c(5,6)),
-                      
-                                     lhei = c(2, nrow(theMatrix)/20 + 3),
-                                     lwid = c(2, ncol(theMatrix)/10 + 3),
-                                     ##lhei=c(2,4,0.2),
-                                     Colv = "Rowv"
-                                     
-                                     
-        )
-        
-        dev.off()
-      }
-    }
+      plots[[i]][[k]] <- heatmap.2(theMatrix)
+      
+      dev.off()
   }
 }
+
+
