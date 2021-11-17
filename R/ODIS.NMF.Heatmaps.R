b320098dfbce4a5a1f72125fb4037f4d0f3aed95
lchrastecka <lucie.chrastecka@stonybrook.edu>
2021-11-17 19:37:48 +0000

    Modifications and adjustment to NMF Heatmap Function

--- R/ODIS.NMF.Heatmaps.R
+++ R/ODIS.NMF.Heatmaps.R
@@ -1,10 +1,10 @@
-#To do:
-#make it such that it selects 'up to top 30' not 'top 30' for both genes and pathways inside k loop
+#To discuss in meeting:
 #create colors based on K and not my_pallette
-#add rownames to theBigMatrix (pathways)
-#make heatmap
-#line 76?
+#make heatmap not suck
 
+res_nmf <- readRDS("./Snyder_NMF_V123.rds")
+gsea_results <- res_nmf
+i <- names(gsea_results[[1]][2])
 
 
 ODIS.NMF.Heatmaps <- function(gsea_results){
@@ -14,11 +14,17 @@ ODIS.NMF.Heatmaps <- function(gsea_results){
     d <- (1-cor(t(x)))
     return(as.dist(d))}
   
+  #original distance fucntion
+  distfun1 =  function(x) {
+    d <- (1-cor(t(x))) + as.matrix(pdist(sign(rowMeans(x))))
+    return(as.dist(d))}
+  
   #define color palette - different color for each K and range of pigment for log2foldchange
   my_palette <- colorRampPalette(c("blue", "lightgrey", "red"))(n = 299)
   
   #initiate list of plots
   plots <- list()
+ 
   
   #this loop glues the small matrixes together
   for(i in names(gsea_results[[1]])){ #for each experimnet V, pull object Vx 
@@ -28,9 +34,10 @@ ODIS.NMF.Heatmaps <- function(gsea_results){
     #this loop extracts top 30genes from each gmt.file and their top 30 associated pathways, and puths them into a clustered matrix
     for(k in names(gsea_results)){
       orig_matrix = gsea_results[[k]]$orig_matrix #pull out and separate for appropriate k every time
-      theseResults <- gsea_results[[k]][[i]]$Results[1:25,] #Selects top 30 enriched pathways and their statsby normalized enrichment score
+      theseResults <- gsea_results[[k]][[i]]$Results
+      theseResults <- theseResults[1:min(30, length(theseResults)),] #Selects up to top 30 enriched pathways and their statsby normalized enrichment score
       theEdge = unique(unlist(theseResults$leadingEdge)) #Takes all the leading edge genes from theseResults
-      theTopGenes = theEdge[order(match(theEdge, orig_matrix$ENTREZID))][1:200] #Returns the row index of theEdge vs orig_matrix, which is sorted by log2FoldChange (decreasing)
+      theTopGenes = theEdge[order(match(theEdge, orig_matrix$ENTREZID))][1:min(30, length(theEdge))] #Returns the row index of theEdge vs orig_matrix, which is sorted by log2FoldChange (decreasing)
       print(length(theTopGenes)) #checks lenght, should be 30
       agg_leading_edge = orig_matrix[orig_matrix$ENTREZID %in% theTopGenes,c("log2FoldChange","ENTREZID")] #Takes the genes corresponding to the index of theTopGenes
       print(dim(agg_leading_edge)) #checks dim of agg_leading_edge, should be 30 x 2
@@ -45,14 +52,18 @@ ODIS.NMF.Heatmaps <- function(gsea_results){
       
       rownames(theMatrix) <- theseResults$pathway #assign rownames (pathways)
       colnames(theMatrix) <- agg_leading_edge$ENTREZID #assign colnames (genes)
-        
+       
       #for each of the leading edges, clean out theMatrix
+      
       for(j in 1:length(theseResults$leadingEdge)){
         tle <- theseResults$leadingEdge[[j]]
-        theMatrix[j, !(colnames(theMatrix) %in% tle)] <- -0.01
+        theMatrix[j, !(colnames(theMatrix) %in% tle)] <- 0.01
       }
       image(t(theMatrix))
-      #Cluster theMatrix by distfun (defined at beginning)
+      
+    #TO DO HERE#  #take standard deviation of the rows and columns, and remove ones that are 0
+      
+      #Cluster theMatrix by distfun (defined at beginning) #refer to this later, before putting thebigmatrix into heatmap
       rowCluster <- hclust(distfun(theMatrix))
       colCluster <- hclust(distfun(t(theMatrix)))
       
@@ -61,19 +72,21 @@ ODIS.NMF.Heatmaps <- function(gsea_results){
         theBigMatrix <- theMatrix
       }else{
         theBigMatrix <- merge(theBigMatrix,theMatrix,by = "row.names",all=TRUE)
-        # get rid of weird columns, then turn it back into a matrix
-        theBigMatrix <- theBigMatrix[,!(names(theBigMatrix) %in% "Row.names")]
-        #add rownames(pathways) to TheBigMatrix
+        rownames(theBigMatrix) <- theBigMatrix$Row.names #adding pathway names into rownames
+        theBigMatrix <- theBigMatrix[,!(names(theBigMatrix) %in% "Row.names")] #removing callumn with pathways names
         theBigMatrix <- as.matrix(theBigMatrix)
       }
+      
       print(k)
       print(typeof(theBigMatrix)) 
       print(str(theBigMatrix))
       print(dim(theBigMatrix))
     }
+    
 image(t(theBigMatrix))    
+
 image(theBigMatrix)
-theBigMatrix[!is.finite(theBigMatrix)] <- 0 #removing NA/NAN/Inf from matrix befroe plotting
+theBigMatrix[!is.finite(theBigMatrix)] <- 0 #removing NA/NAN/Inf from matrix before plotting
 
 # heatmap it, it should already be clustered
       
@@ -86,12 +99,12 @@ theBigMatrix[!is.finite(theBigMatrix)] <- 0 #removing NA/NAN/Inf from matrix bef
       
       plots[[i]][[k]] <- heatmap.2(theBigMatrix,
                                    main = paste0(i, "_", substr(names(thisAnalysis)[k], 5, stop = 999)),
-                                   hclustfun = distfun,
-                                   #distfun = distfun,
+                                   #hclustfun = colCluster,
+                                   distfun = rowCluster,
                                    density.info = "histogram",
                                    dendrogram = "none",
                                    trace = "none",
-                                   margins = c(3,20),
+                                   #margins = c(3,20),
                                    cexRow = 0.7,
                                    cexCol = 0.5,
                                    col = my_palette, #color by thisAnalysis[k]
