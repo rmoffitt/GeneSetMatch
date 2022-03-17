76e615f32c59ef167ddc044e0ac10f51dfb9ce34
lchrastecka <lucie.chrastecka@stonybrook.edu>
2022-03-17 20:16:34 +0000

    Updates to function

--- R/ODIS.Multilevel.Heatmaps.R
+++ R/ODIS.Multilevel.Heatmaps.R
@@ -1,4 +1,4 @@
-# This function generates a Hierarchically clustered Multilevel Heatmap which displays three samples in one concise plot.
+# This function generates a Hierarchically clustered Multilevel Heatmaps which displays three samples in one concise plot.
 #' This function takes direct GSEA output and transforms it into a unique visualization
 #' @export
 #' @import pdist
@@ -9,7 +9,7 @@
 #' @import plotly
 #' @import matrixStats
 #' @param gsea_results is the direct output from GSEA function in step immediately prior. There is no need for restructuring, just feed it directly into the function as is. 
-#' @return multi-colored heatmaps displaying 3 experiemntal conditions/samples/vectors
+#' @return multi-colored heatmaps displaying 3 experimental conditions/samples/vectors
 #' @examples 
 #' gsea_results is GSEA result
 #' ODIS.Multilevel.Heatmaps(gsea_results)
@@ -34,35 +34,43 @@ ODIS.Multilevel.Heatmaps <- function(gsea_results){
     d <- (1-cor(t(x)))
     return(as.dist(d))}
   
+  #initiate list of plots
+  plots <- list()
+  filelist <- list()  
+  
   #original distance fucntion
   #library(pdist)
   # distfun1 =  function(x) {
   #   d <- (1-cor(t(x))) + as.matrix(pdist(sign(rowMeans(x))))
   #   return(as.dist(d))}
   
-  #initiate list of plots
-  plots <- list()
-  filelist <- list()
-  
   #this loop glues the small matrixes together
   for(i in names(gsea_results[[1]])){ #for each experiment V, pull object Vx 
     thisAnalysis <- gsea_results[[i]]
-    theBigMatrix = list() #every time I make a small matrix for each experiment (k), glue them together here
-    
+    theBigMatrix <- list() #every time I make a small matrix for each experiment (k), glue them together here
+    plots[[i]] <- list()
     #this loop extracts top 30genes from each gmt.file and their top 30 associated pathways, and puts them into a clustered matrix
     for(k in names(gsea_results)){
+      print('starting loop with:')
+      print(i)
+      print(k)
+      
       orig_matrix = gsea_results[[k]]$orig_matrix #pull out and separate for appropriate k every time
       theseResults <- gsea_results[[k]][[i]]$Results
-      # if (length(theseResults == 0)){
-      #   next
-      # }
+      if (is.na(theseResults[1,1]) == T) {
+         print("These results are EMPTY!!!")
+         next
+      }
       theseResults <- theseResults[1:min(30, nrow(theseResults)),] #Selects up to top 30 enriched pathways and their stats by normalized enrichment score
       theEdge = unique(unlist(theseResults$leadingEdge)) #Takes all the leading edge genes from theseResults
       theTopGenes = theEdge[order(match(theEdge, orig_matrix$ENTREZID))][1:min(30, length(theEdge))] #Returns the row index of theEdge vs orig_matrix, which is sorted by log2FoldChange (decreasing)
-      print(length(theTopGenes)) #checks lenght, should be 30
+      #print('theTopGenes:')
+      #print(length(theTopGenes)) #checks length, should be 30
       agg_leading_edge = orig_matrix[orig_matrix$ENTREZID %in% theTopGenes,c("log2FoldChange","ENTREZID")] #Takes the genes corresponding to the index of theTopGenes
-      print(dim(agg_leading_edge)) #checks dim of agg_leading_edge, should be 30 x 2
-      
+      #print('agg_leading_edge:')
+      #print(str(agg_leading_edge)) #checks dim of agg_leading_edge, should be 30 x 2
+      #print('theseResults$pathway:')
+      #print(str(theseResults$pathway)) #checks dim of agg_leading_edge, should be 30 x 2
       #make a small matrix of agg_leading_edge and theseResults$pathways for each k
       theMatrix <- matrix(data = 1,
                           byrow = T,
@@ -75,19 +83,27 @@ ODIS.Multilevel.Heatmaps <- function(gsea_results){
       colnames(theMatrix) <- agg_leading_edge$ENTREZID #assign colnames (genes)
       
       #for each of the leading edges, clean out theMatrix
+      print('theMatrix:')
+      print(str(theMatrix))
       
       for(j in 1:length(theseResults$leadingEdge)){
+        #print('j:')
+        #print(j)
         tle <- theseResults$leadingEdge[[j]]
         theMatrix[j, !(colnames(theMatrix) %in% tle)] <- 0
       }
-      image(t(theMatrix))
+      #image(t(theMatrix))
       
+      
+      #rowSD changed to eowMeans etc
       #take standard deviation of the rows and columns
-      theMatrix_row_std = rowSds(theMatrix) # create a vector with all rowSds, keeps the order of rows in theMatrix
-      theMatrix_col_std = colSds(theMatrix)
+      theMatrix_row_std = rowMeans(theMatrix) # create a vector with all rowSds, keeps the order of rows in theMatrix
+      theMatrix_col_std = colMeans(theMatrix)
       
+      #print(theMatrix_row_std)
+      #print(theMatrix_col_std)
       #remove all rows and cols that have less stdev that 0.01 to avid empty space in plot
-      theMatrix <- theMatrix[!(theMatrix_row_std < 0.01), !(theMatrix_col_std < 0.01)]
+      theMatrix <- theMatrix[!(theMatrix_row_std == 0), !(theMatrix_col_std == 0)]
       
       #somewhox .x and .y is added to genenames in this merge, because genes show up in more than one V, once this is fixed 129 should work
       if(typeof(theBigMatrix)=="list"){
@@ -98,64 +114,62 @@ ODIS.Multilevel.Heatmaps <- function(gsea_results){
         rownames(theBigMatrix) <- theBigMatrix$Row.names #adding pathway names into row names
         theBigMatrix <- theBigMatrix[,!(names(theBigMatrix) %in% "Row.names")] #removing column with pathways names
         theBigMatrix <- as.matrix(theBigMatrix)
+        theBigMatrix[!is.finite(theBigMatrix)] <- 0 #removing NA/NAN/Inf from matrix before plotting
+        #image(t(theBigMatrix))
+        redundantX <- grep(pattern = ".x", colnames(theBigMatrix), value = T)
+        redundantX <- gsub(pattern = "\\.x", replacement = "", x = redundantX)
       }
       
-      print(k)
-      print(typeof(theBigMatrix)) 
+      print('theBigMatrix at end of loop:')
       print(str(theBigMatrix))
-      print(dim(theBigMatrix))
     }
-    
-    theBigMatrix[!is.finite(theBigMatrix)] <- 0 #removing NA/NAN/Inf from matrix before plotting
-    image(t(theBigMatrix))
-    
-    redundantX <- grep(pattern = ".x", colnames(theBigMatrix), value = T)
-    redundantX <- gsub(pattern = "\\.x", replacement = "", x = redundantX)
+      
+    print('k is done looping over V1, V2 etc. . . ')
+    print('the matrix of genes and genesets has been constructed:')
+    print(str(theBigMatrix))
     
     for (z in redundantX){
-      indX <- grep(colnames(theBigMatrix), pattern = z) #find the col indices of .x and .y
-      theBigMatrix[which(theBigMatrix[,indX[2]] == 1), #"if there is a 1 value in the ".y", make it a 1 in the ".x"
-                   indX[1]] = 1
-      theBigMatrix = theBigMatrix[, -indX[2]] #remove the .y column
-      colnames(theBigMatrix)[indX[1]] = z #remove the .x by replacing it with original z
+        indX <- grep(colnames(theBigMatrix), pattern = z) #find the col indices of .x and .y
+        theBigMatrix[which(theBigMatrix[,indX[2]] == 1), #"if there is a 1 value in the ".y", make it a 1 in the ".x"
+                     indX[1]] = 1
+        theBigMatrix = theBigMatrix[, -indX[2]] #remove the .y column
+        colnames(theBigMatrix)[indX[1]] = z #remove the .x by replacing it with original z
     }
     
+    print('theBigMatrix is clean')
+    image(theBigMatrix)    
     #re-order theBigMatrix ()
     rowCluster <- hclust(distfun(theBigMatrix))
     colCluster <- hclust(distfun(t(theBigMatrix)))
     
-    image(t(theBigMatrix))  
+        
     theBigMatrixOrdered <- theBigMatrix[rowCluster$order, colCluster$order] #ordered rows and columns by rowClust$order
     image(t(theBigMatrixOrdered))
-    
-    #This forloop determines RGB triplet for each gene, builds corresponding matrixes and plots a raster
+        
+    #This forloop determines RGB triplet for each gene, builds corresponding matrices and plots a raster
     rbgmatrix <- matrix(data = "#FFFFFF",
-                        ncol = length(colnames(theBigMatrixOrdered)),
-                        nrow = length(rownames(theBigMatrixOrdered)))
-    
+                            ncol = length(colnames(theBigMatrixOrdered)),
+                            nrow = length(rownames(theBigMatrixOrdered)))
+        
     colnames(rbgmatrix) = colnames(theBigMatrixOrdered)
     rownames(rbgmatrix) = rownames(theBigMatrixOrdered)
-    
-    for(setID in 1:nrow(theBigMatrixOrdered)){
-      for(geneID in 1:ncol(theBigMatrixOrdered)){
-        tbmo <- theBigMatrixOrdered[setID, geneID]
-        print(tbmo)
         
-        if (tbmo == 0){
-          
-          rbgmatrix[setID, geneID] <- "#000000" 
+    for(setID in 1:nrow(theBigMatrixOrdered)){
+        for(geneID in 1:ncol(theBigMatrixOrdered)){
+            tbmo <- theBigMatrixOrdered[setID, geneID]
+            if (tbmo == 0){
+              rbgmatrix[setID, geneID] <- "#000000" 
+            }
+            else {e <- colnames(theBigMatrixOrdered)[geneID]
+            r <- res_nmf$V1$orig_matrix[e,"log2FoldChange"] 
+            g <- res_nmf$V2$orig_matrix[e,"log2FoldChange"]
+            b <- res_nmf$V3$orig_matrix[e,"log2FoldChange"]
+            rbgmatrix[setID, geneID] <- rgb(r, g, b)
+            }
         }
-        else {e <- colnames(theBigMatrixOrdered)[geneID]
-        r <- res_nmf$V1$orig_matrix[e,"log2FoldChange"] 
-        g <- res_nmf$V2$orig_matrix[e,"log2FoldChange"]
-        b <- res_nmf$V3$orig_matrix[e,"log2FoldChange"]
-        rbgmatrix[setID, geneID] <- rgb(r, g, b)
-        }
-      }
     }
     
-    
-    #make tall table of x, y, color for ggplot, use plotly 
+        #make tall table of x, y, color for ggplot, use plotly 
     tallmatrix <- as.character(rbgmatrix)
     tallmatrix <- data.frame(data = tallmatrix,
                              x = rep(1:dim(rbgmatrix)[2], each = dim(rbgmatrix)[1]),
@@ -165,27 +179,25 @@ ODIS.Multilevel.Heatmaps <- function(gsea_results){
     xlabels <- c(colnames(rbgmatrix))
     ylabels <- c(rownames(rbgmatrix))
     
-    print(paste0(i, "_", substr(names(thisAnalysis)[k],
-                                5, stop = 999), ".pdf"))
-    filelist[[i]][[k]] <- paste0(i, "_", substr(names(thisAnalysis)[k],
-                                                5, stop = 999), ".pdf")
+    print(paste0(i, ".pdf"))
+    filelist[[i]] <- paste0(i, ".pdf")
     
-    pdf(paste0(i, "_", substr(names(thisAnalysis)[k],
-                              5, stop = 999), ".pdf"),
-        width = ncol(rbgmatrix)/10 + 5,
-        height= nrow(rbgmatrix)/10 + 5)
+    pdf(paste0(i, ".pdf"),
+        width = ncol(rbgmatrix),
+        height= nrow(rbgmatrix))
     
-    
-    plots[[i]][[k]] <- ggplot(tallmatrix) +
+    plots[[i]] <- ggplot(tallmatrix) +
       geom_raster(aes(x = x, y= y, fill = data)) +
       scale_fill_identity() +
       scale_y_continuous(labels = ylabels, breaks = 1:length(ylabels)) +
       scale_x_continuous(labels = xlabels, breaks = 1:length(xlabels)) +
       theme(axis.text.x = element_text(angle = 45, vjust = 0.5, hjust=1)) +
-      ggtitle(paste0(i, "_", substr(names(thisAnalysis)[k], 1, stop = 999)))
+      ggtitle(paste0(i))
     
-    
+    print(plots[[i]])
     dev.off()
   }
+  
   return(filelist)
 }
+
