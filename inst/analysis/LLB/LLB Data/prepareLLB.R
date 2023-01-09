296ae72a2ddabb163ed3c9868bcc53298311b73d
lchrastecka <lucie.chrastecka@stonybrook.edu>
2023-01-09 03:50:48 +0000

    LLB PIPELINE FROM RAW

---
+++ inst/analysis/LLB/LLB Data/prepareLLB.R
@@ -0,0 +1,25 @@
+prepareLLB <- function(){
+  require(annotate)
+  require(rat2302.db) #affymetrix chip for liverlungbrain
+  LLB <- readRDS("./LiverLungBrain.rds")
+  
+  ex <- as.data.frame(LLB$ex)
+  
+  genes <- rownames(ex)
+  ex$PROBEID <- rownames(ex)
+  convert <- AnnotationDbi::select(rat2302.db, genes, c("SYMBOL","ENTREZID", "ENSEMBL")) 
+  convert <- convert[match(unique(convert$SYMBOL), convert$SYMBOL),]
+  ex <- merge(ex, convert, "PROBEID")
+  ex <- ex[which(!is.na(ex$ENTREZID)),]
+  #rownames(W) <- W$ENTREZID
+  rownames(ex) <- ex$ENTREZID
+  ex <- dplyr::select(ex,!c("SYMBOL","ENTREZID", "ENSEMBL", "PROBEID"))
+  condition <- c(rep("liver", 3), rep("brain", 3), rep("lung", 3), rep("mixed", 33))
+  
+  LLB <- list(ex = ex, condition= condition)
+  LLB$species <- "Rattus norvegicus"
+  
+  #detach(package:annotate,unload=TRUE)
+  #detach(package:rat2302.db,unload=TRUE)
+  saveRDS(LLB, file = "~/ODIS2/inst/analysis/LLB/LLB Data/LLB Data.Rds")
+}
